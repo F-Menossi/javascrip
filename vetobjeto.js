071ac9b097d0533f36dd48e0b0f969df07e3fb95
FELIPE AUGUSTO DE PADUA MENOSSI <21793@unifacef.edu.br>
2019-08-21 21:34:51 -0300

    exemplo de vetor de objetos v 0.1

---
+++ vetobjeto.js
@@ -0,0 +1,13 @@
+function funcao(){
+    var = objeto{}
+    for (var i=0;i<5; i++){
+        objeto.codigo = Number(prompt("Informe código do produto: "))
+        objeto.nome = pompt("Informe nome do produto: ")
+        objeto.qtde = Number(prompt("Informe quantidade do produto: "))
+        objeto.preco = Number(prompt("Informe preço do produto: "))
+        vet.push(objeto)
+        alert("Produto criado com sucesso " + objeto.qtde)
+    }
+    var estoque = 0
+    for(var i=0;i<5;i++)
+}
